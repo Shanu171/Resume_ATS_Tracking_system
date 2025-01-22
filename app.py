b3c8850ad60dc7e3f76bb26a734990e1227941dc
Shanu171 <45221019+Shanu171@users.noreply.github.com>
2025-01-22 11:21:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
-from dotenv import load_dotenv
 
-load_dotenv()
-
-
-import streamlit as st 
 import os 
-import pdf2image 
-from PIL import Image
-import google.generativeai as genai 
 import io
 import base64
+import pdf2image 
+import PyPDF2 as pdf
+from PIL import Image
+import streamlit as st 
+import google.generativeai as genai 
+from dotenv import load_dotenv
+load_dotenv()
 
-
-
-
+# Configuration of API 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def  get_gemini_response(input,pdf_content,prompt):
@@ -43,10 +40,7 @@ if uploaded_file is not None:
 
 
 submit1 = st.button("Tell me about the Resume")
-
-#submit2 = st.button("How can I Improvise my Skills")
-
-submit3 = st.button("Percentage Match")
+submit2 = st.button("Percentage Match")
 
 
 input_prompt1 = """
@@ -62,16 +56,13 @@ affect their suitability for the role.
 """
 
 
-input_prompt3 = """
+input_prompt2 = """
 You are a highly skilled ATS (Applicant Tracking System) evaluator with deep expertise in technical fields such as Data Science, Full Stack Java Development, Full Stack Web Development, Big Data Engineering, Data Engineering, and Data Analysis. You also possess a strong understanding of ATS functionalities.
-
 Your task is to analyze the provided resume in comparison to the job description. Calculate the percentage match between the resume and the job description. 
-
 In your output:
 1. Begin by providing the overall match percentage.
 2. List any key skills, keywords, or qualifications present in the job description that are missing in the resume. Ensure these are detailed and categorized (e.g., technical skills, certifications, or experience).
 3. Conclude with actionable recommendations for improving the resume to better align with the job description.
-
 """
 
 if  submit1: 
@@ -84,10 +75,10 @@ if  submit1:
     else:
         st.write("Please upload the Resume")
 
-elif submit3:
+elif submit2:
     if uploaded_file is  not None:
         pdf_content =   input_pdf_setup(uploaded_file)
-        response = get_gemini_response(input_prompt3,pdf_content,input_text)
+        response = get_gemini_response(input_prompt2,pdf_content,input_text)
         st.subheader("The Response is")
         st.write(response)
     else:
